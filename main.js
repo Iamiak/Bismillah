3347885cd52da1a5f52e759362bb45ae55a69e27
Iamiakk <153729760+Iamiak@users.noreply.github.com>
2024-03-27 23:27:40 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.once('ready', () => {
-    console.log('Bot is online');
-    client.user.setStatus('online');
-});
-
-client.login('MTIyMjYwMzgzMTUyMzY3NjMwMg.GKSeHp._uoY3PM8DVvr1KzMLgOsGg6WAS1ZTP2bSGsP-Q');
